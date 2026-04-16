fff184f3782c05bffb11fb9262ecd96e73d2cc90
Manuel Munizaga Sepulveda <manuel.chile@yahoo.com>
2026-04-16 19:58:08 +0800

    Create influential_agents.py

---
+++ experiments/Base/influential_agents.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+import src.GATOH.agents as agt
+import src.GATOH.graphs as gr
+import src.GATOH.logging as lg
+import src.GATOH.model as md
+
+
+class InfluentialTester:
+    def __init__(self):
+        pass
+
+    def create_li_agents(self):
+        pass
+
+    def create_hi_agents(self):
+        pass
+
+    def create_li_graphs(self):
+        pass
+
+    def create_hi_graphs(self):
+        pass
+
+    def run_model_li(self):
+        pass
+
+    def run_model_hi(self):
+        pass
+
+
+if __name__ == "__main__":
+    tester: InfluentialTester = InfluentialTester()
